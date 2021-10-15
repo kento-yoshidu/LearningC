963e108271f628fc97d2d08d14e4dd650ba468a6
kento <kento@toriwatari.bird>
2021-10-15 21:59:31 +0900

    😜 Update

---
+++ chap01/list0108.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void) {
+  int x, y;
+
+  x = 57;
+  y = x + 13;
+
+  printf("x = %d, y = %d", x, y);
+
+  return 0;
+}
---
+++ chap01/list0111.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void) {
+  int no;
+
+  printf("入力してください");
+  scanf("%d", &no);
+
+  printf("%d", no);
+
+  return 0;
+}
